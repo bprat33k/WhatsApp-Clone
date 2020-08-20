01e6cbffd3e4e1ea19ec2f98106b2c0836ed679a
drivecrux <prateekbajpai444@gmail.com>
2020-08-20 13:15:23 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
